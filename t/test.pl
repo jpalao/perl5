fb2978d536f6958cb3a3dc53a7c9fef08265a347
Jose Palao <dr.palao@gmail.com>
2021-03-07 23:45:53 +0100

    test fixes

--- t/test.pl
+++ t/test.pl
@@ -38,7 +38,7 @@ our $Tests_Are_Passing = 1;
 
 use Cwd qw/getcwd/;
 use cbrunperl;
-$cbrunperl::DEBUG = 1;
+$cbrunperl::DEBUG = 0;
 
 # Use this instead of print to avoid interference while testing globals.
 sub _print {
@@ -1482,7 +1482,7 @@ sub run_multiple_progs {
 	    }
 
 	    if ($ok && $fatal && !($status >> 8)) {
-		$ok = 0;
+		    $ok = 0 unless is_darwin_ios();
 	    }
 	}
 
