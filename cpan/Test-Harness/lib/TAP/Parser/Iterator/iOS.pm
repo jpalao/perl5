e71e4e162e8cf8fa21c036a5c28ad88686a3f9fe
jose <jpalao@personasytecnicas.com>
2026-09-12 15:38:44 +0200

    Fix focused iOS harness output

--- cpan/Test-Harness/lib/TAP/Parser/Iterator/iOS.pm
+++ cpan/Test-Harness/lib/TAP/Parser/Iterator/iOS.pm
@@ -92,8 +92,6 @@ sub _initialize {
         ($exit_code, $tap) = exec_test($workdir, $command_parts);
         $teardown->() if $teardown;
         if (defined $tap) {
-            print STDOUT $tap if $ENV{PERL_FOUNDATION_RUNNER}
-                && $ENV{PERL_FOUNDATION_RUNNER_VERBOSE};
             utf8::downgrade($tap, 1) if utf8::is_utf8($tap);
             utf8::decode($tap) unless utf8::is_utf8($tap);
         }
