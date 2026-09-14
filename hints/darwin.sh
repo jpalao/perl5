c77dc50afe7966623f435c9a4a57039a8a347dba
jose <jpalao@personasytecnicas.com>
2026-09-14 22:47:07 +0200

    Fix physical iOS Configure transfer flags

--- hints/darwin.sh
+++ hints/darwin.sh
@@ -300,23 +300,29 @@ case "$osvers" in  # Note: osvers is the kernel version, not the 10.x
 
    # We now use MACOSX_DEPLOYMENT_TARGET, if set, as an override by
    # capturing its value and adding it to the flags.
-    case "$MACOSX_DEPLOYMENT_TARGET" in
-    [1-9][0-9].*)
-      add_macosx_version_min ccflags $MACOSX_DEPLOYMENT_TARGET
-      add_macosx_version_min ldflags $MACOSX_DEPLOYMENT_TARGET
-      ;;
-    '')
-      # Empty MACOSX_DEPLOYMENT_TARGET is okay.
+    case "$targetrun" in
+    darwin-ios)
       ;;
     *)
-      cat <<EOM >&4
+      case "$MACOSX_DEPLOYMENT_TARGET" in
+      [1-9][0-9].*)
+        add_macosx_version_min ccflags $MACOSX_DEPLOYMENT_TARGET
+        add_macosx_version_min ldflags $MACOSX_DEPLOYMENT_TARGET
+        ;;
+      '')
+        # Empty MACOSX_DEPLOYMENT_TARGET is okay.
+        ;;
+      *)
+        cat <<EOM >&4
 
 *** Unexpected MACOSX_DEPLOYMENT_TARGET=$MACOSX_DEPLOYMENT_TARGET
 ***
 *** Please either set it to a valid macOS version number (e.g., 10.15) or to empty.
 
 EOM
-      exit 1
+        exit 1
+        ;;
+      esac
       ;;
     esac
 
