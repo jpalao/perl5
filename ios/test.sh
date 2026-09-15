55a0ffcfb47eb7b79062a31faf131ab78fd7d16d
jose <jpalao@personasytecnicas.com>
2026-09-15 16:37:18 +0200

    Stream foundation runner output on macOS

--- ios/test.sh
+++ ios/test.sh
@@ -85,6 +85,7 @@ PERL_TEST_LOG=""
 TEST_LOG_SOURCE=""
 TEST_STATUS_SOURCE=""
 TRANSFER_TRANSPORT=""
+FOUNDATION_RUNNER_LOCAL_LOG=0
 DEVICECTL_AVAILABLE=0
 DEVICECTL_CONNECTED=0
 IOS_DEPLOY_AVAILABLE=0
@@ -630,7 +631,16 @@ launch_harness_with_idevicedebug() {
         return 1
     }
     echo "Launching $HARNESS_APP_ID with idevicedebug on $IOS_DEVICE_UUID"
-    if idevicedebug -u "$IOS_DEVICE_UUID" run "$HARNESS_APP_ID"; then
+    if [ "$IOS_TEST_APP" = "foundation-runner" ] || [ "$IOS_TEST_APP" = "runner" ]; then
+        : > "$PERL_TEST_LOG"
+        idevicedebug -u "$IOS_DEVICE_UUID" run "$HARNESS_APP_ID" 2>&1 | tee -a "$PERL_TEST_LOG"
+        local debug_status=${PIPESTATUS[0]}
+        if [ "$debug_status" -eq 0 ]; then
+            FOUNDATION_RUNNER_LOCAL_LOG=1
+            echo "idevicedebug launch succeeded"
+            return 0
+        fi
+    elif idevicedebug -u "$IOS_DEVICE_UUID" run "$HARNESS_APP_ID"; then
         echo "idevicedebug launch succeeded"
         return 0
     fi
@@ -893,6 +903,11 @@ test_perl_device() {
             perl -e "while (1) {sleep 1; system qw (ls $IOS_MOUNTPOINT);} " > /dev/null 2>&1 &
             MOUNT_REFRESH_PID=$!
         fi
+    fi
+
+    if [ "$FOUNDATION_RUNNER_LOCAL_LOG" -eq 1 ]; then
+        echo "Foundation runner output captured on macOS: $PERL_TEST_LOG"
+        return 0
     fi
 
     rm -f "$PERL_TEST_LOG"
--- ios/test/foundation-runner/main.m
+++ ios/test/foundation-runner/main.m
@@ -1,7 +1,6 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <ios/ios.h>
-#import <unistd.h>
 
 static NSString *RunnerArgument(NSArray *arguments, NSString *name, NSString *fallback) {
     NSUInteger index = [arguments indexOfObject:name];
@@ -11,7 +10,7 @@ static NSString *RunnerArgument(NSArray *arguments, NSString *name, NSString *fa
     return fallback;
 }
 
-static int RunPerlScript(NSString *scriptPath, NSString *outputPath, NSString *statusPath) {
+static int RunPerlScript(NSString *scriptPath, NSString *statusPath) {
     __block int normalizedResult = 255;
     @autoreleasepool {
         setenv("PERL_FOUNDATION_RUNNER", "1", 1);
@@ -19,57 +18,8 @@ static int RunPerlScript(NSString *scriptPath, NSString *outputPath, NSString *s
         NSURL *scriptURL = [NSURL fileURLWithPath:scriptPath];
         NSString *workingDirectory = [[scriptURL URLByDeletingLastPathComponent] path];
         __block int perlResult = 255;
-        NSPipe *stdoutPipe = [NSPipe pipe];
-        NSPipe *stderrPipe = [NSPipe pipe];
         NSMutableDictionary *perlDictionary = [[NSMutableDictionary alloc] initWithCapacity:128];
         [PerlCtrl initPerlInstanceDictionary:perlDictionary];
-        int savedStdout = dup(STDOUT_FILENO);
-        int savedStderr = dup(STDERR_FILENO);
-        dispatch_group_t pipeReaders = dispatch_group_create();
-        NSMutableString *capturedOutput = [[NSMutableString alloc] init];
-
-        if (savedStdout < 0 || savedStderr < 0 ||
-            dup2(stdoutPipe.fileHandleForWriting.fileDescriptor, STDOUT_FILENO) < 0 ||
-            dup2(stderrPipe.fileHandleForWriting.fileDescriptor, STDERR_FILENO) < 0) {
-            NSLog(@"Could not redirect stdout/stderr: %s", strerror(errno));
-            return 1;
-        }
-        close(stdoutPipe.fileHandleForWriting.fileDescriptor);
-        close(stderrPipe.fileHandleForWriting.fileDescriptor);
-        setvbuf(stdout, NULL, _IONBF, 0);
-        setvbuf(stderr, NULL, _IONBF, 0);
-
-        void (^drainPipe)(NSPipe *, int) = ^(NSPipe *pipe, int outputDescriptor) {
-            dispatch_group_enter(pipeReaders);
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-                @autoreleasepool {
-                    while (YES) {
-                        NSData *data = pipe.fileHandleForReading.availableData;
-                        if (data.length == 0) {
-                            break;
-                        }
-                        NSString *text = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] ?: @"";
-                        @synchronized (capturedOutput) {
-                            [capturedOutput appendString:text];
-                        }
-                        const uint8_t *bytes = data.bytes;
-                        size_t remaining = data.length;
-                        while (remaining > 0) {
-                            ssize_t written = write(outputDescriptor, bytes, remaining);
-                            if (written <= 0) {
-                                break;
-                            }
-                            bytes += written;
-                            remaining -= (size_t)written;
-                        }
-                    }
-                }
-                dispatch_group_leave(pipeReaders);
-            });
-        };
-
-        drainPipe(stdoutPipe, savedStdout);
-        drainPipe(stderrPipe, savedStderr);
 
         dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
             @autoreleasepool {
@@ -81,28 +31,10 @@ static int RunPerlScript(NSString *scriptPath, NSString *outputPath, NSString *s
                                withArguments:nil
                                        error:&error
                                   completion:^(int result) {
-                    fflush(stdout);
-                    fflush(stderr);
                     perlResult = result;
                 }];
             }
         });
-
-        dup2(savedStdout, STDOUT_FILENO);
-        dup2(savedStderr, STDERR_FILENO);
-
-        dispatch_group_wait(pipeReaders, DISPATCH_TIME_FOREVER);
-        close(savedStdout);
-        close(savedStderr);
-
-        NSError *outputError = nil;
-        [capturedOutput writeToFile:outputPath atomically:YES encoding:NSUTF8StringEncoding error:&outputError];
-        if (capturedOutput.length > 0) {
-            NSLog(@"Perl output captured");
-        }
-        if (outputError != nil) {
-            NSLog(@"Could not write Perl output: %@", outputError.localizedDescription);
-        }
 
         if (error != nil) {
             if (perlResult == 0) {
@@ -153,13 +85,11 @@ static int RunPerlScript(NSString *scriptPath, NSString *outputPath, NSString *s
                                                                  YES) firstObject];
     NSString *fullTestScript = [documents stringByAppendingPathComponent:@"t/ios_harness"];
     NSString *scriptPath = RunnerArgument(arguments, @"--script", fullTestScript);
-    NSString *defaultOutput = [documents stringByAppendingPathComponent:@"perl-tests.txt"];
-    NSString *outputPath = RunnerArgument(arguments, @"--output", defaultOutput);
     NSString *defaultStatus = [documents stringByAppendingPathComponent:@"perl-tests.status"];
     NSString *statusPath = RunnerArgument(arguments, @"--status", defaultStatus);
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)),
                     dispatch_get_global_queue(QOS_CLASS_UTILITY, 0), ^{
-        int result = RunPerlScript(scriptPath, outputPath, statusPath);
+        int result = RunPerlScript(scriptPath, statusPath);
         exit(result);
     });
 }
