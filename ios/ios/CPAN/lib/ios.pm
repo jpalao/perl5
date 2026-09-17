e651ff90e0a6526cfa0eedac4095d4839607a3fc
jose <jpalao@personasytecnicas.com>
2026-09-17 20:15:48 +0200

    iOS: remove ios module

--- ios/ios/CPAN/lib/ios.pm
+++ ios/ios/CPAN/lib/ios.pm
@@ -166,6 +166,12 @@ sub _perl_switches_with_environment {
         my ($switches) = @_;
         my @result = @{$switches || []};
 
+        if ($^O =~ /darwin-ios/
+            && !grep { /^-Mios(?:=|\z)/ } @result) {
+            my $index = @result && $result[0] =~ /^-[Tt]\z/ ? 1 : 0;
+            splice @result, $index, 0, '-Mios';
+        }
+
         return \@result if grep { $_ eq '-T' || $_ eq '-t' } @result;
         return \@result if !defined $ENV{PERL5LIB};
 
